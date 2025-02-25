0aabdbb299a1bc6ad9bd4ace4f0705f1203ca37b
ANGEL_GABRIEL_ALVARADO_AGUIRRE <a.22308051281127@cbtis128.edu.mx>
2025-02-25 16:58:44 +0000

    Alvarado img

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              "Mis Imagenes",
+            ),
+            backgroundColor: Colors.redAccent,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Angel Alvarado Mat: 22308051281127',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black26,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                // Primera imagen desde la red
+                Image.network(
+                  'https://raw.githubusercontent.com/GabrielAlvrado89/Imagenes-para-App-flutter-6J/refs/heads/main/dulce1.jpg',
+                  width: 200, // Ancho opcional
+                  height: 200, // Alto opcional
+                  fit: BoxFit.cover, // Ajuste opcional
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Dulces a mitad de precio',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20), // Espacio entre las imágenes
+                // Segunda imagen desde la red
+                Image.network(
+                  'https://raw.githubusercontent.com/GabrielAlvrado89/Imagenes-para-App-flutter-6J/refs/heads/main/dulce2.jpg',
+                  width: 200, // Ancho opcional
+                  height: 200, // Alto opcional
+                  fit: BoxFit.cover, // Ajuste opcional
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  '¡¡Dulces baratos!!',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          )),
     );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
-  }
-}
+  } // widget build
+} // clase mi Imagen
